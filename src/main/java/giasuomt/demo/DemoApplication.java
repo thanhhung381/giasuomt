6eeeb66b3074c98b9412f19119492fe439818ece
thanhhung381 <thanhhung.vl.381@gmail.com>
2021-10-30 09:00:56 +0700

    bổ sung file application

--- src/main/java/giasuomt/demo/DemoApplication.java
+++ src/main/java/giasuomt/demo/DemoApplication.java
@@ -9,35 +9,37 @@ import giasuomt.demo.commondata.util.DateTimeUtils;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
-		/*
-		 * SpringApplication.run(DemoApplication.class, args); String localDateTime =
-		 * DateTimeUtils.toString(LocalDateTime.now());
-		 * System.out.println(localDateTime);
-		 * 
-		 * String[] sep = localDateTime.split("-");
-		 * 
-		 * String year = sep[0];
-		 * 
-		 * String month = sep[1];
-		 * 
-		 * String dateAndTime=sep[2];
-		 * 
-		 * String dateArrayString[]=dateAndTime.split(" ");
-		 * 
-		 * String date=dateArrayString[0];
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * System.out.println(year.substring(2,4));
-		 */
-
-		
-
+		SpringApplication.run(DemoApplication.class, args);
 	}
+//	public static void main(String[] args) {
+//		/*
+//		 * SpringApplication.run(DemoApplication.class, args); String localDateTime =
+//		 * DateTimeUtils.toString(LocalDateTime.now());
+//		 * System.out.println(localDateTime);
+//		 * 
+//		 * String[] sep = localDateTime.split("-");
+//		 * 
+//		 * String year = sep[0];
+//		 * 
+//		 * String month = sep[1];
+//		 * 
+//		 * String dateAndTime=sep[2];
+//		 * 
+//		 * String dateArrayString[]=dateAndTime.split(" ");
+//		 * 
+//		 * String date=dateArrayString[0];
+//		 * 
+//		 * 
+//		 * 
+//		 * 
+//		 * 
+//		 * System.out.println(year.substring(2,4));
+//		 */
+//
+//		
+//
+//	}
 	
 	
 
