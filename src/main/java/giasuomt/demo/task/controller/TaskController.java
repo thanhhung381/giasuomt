691da1c12fcc4a2a3d2fc1c1d84b679ebee9201d
thanhhung381 <thanhhung.vl.381@gmail.com>
2021-11-09 19:57:19 +0700

    test demo websocket

--- src/main/java/giasuomt/demo/task/controller/TaskController.java
+++ src/main/java/giasuomt/demo/task/controller/TaskController.java
@@ -1,18 +1,17 @@
 package giasuomt.demo.task.controller;
-
-import java.util.List;
-
+import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import giasuomt.demo.commondata.generic.GenericController;
-import giasuomt.demo.commondata.generic.IGenericService;
 import giasuomt.demo.commondata.responseHandler.ResponseHandler;
-import giasuomt.demo.task.dto.ResponseTaskDto;
 import giasuomt.demo.task.dto.SaveTaskDto;
 import giasuomt.demo.task.model.Task;
 import giasuomt.demo.task.service.ITaskService;
@@ -22,5 +21,15 @@ import lombok.AllArgsConstructor;
 @RestController
 @AllArgsConstructor
 public class TaskController extends GenericController<SaveTaskDto, Task, Long, BindingResult> {
+	private ITaskService iGenericService;
 	
+	@PostMapping("/create")
+	public ResponseEntity<Object> create(@Valid @RequestBody SaveTaskDto dto, BindingResult errors) {
+		if (((Errors) errors).hasErrors())
+			return ResponseHandler.getResponse(errors, HttpStatus.BAD_REQUEST);
+		
+		Task createdT = iGenericService.create(dto);
+		
+		return ResponseHandler.getResponse(createdT, HttpStatus.OK);
+	}
 }
--- src/main/resources/static/js/main-staff-api.js
+++ src/main/resources/static/js/main-staff-api.js
@@ -20,17 +20,28 @@ const renderTasks = () => {
 };
 
 const fetchTasks = () => {
-    axios({
-        url: 'http://localhost:8080/api/task/findAll',
-        method: 'GET',
-    })
-    .then((res) => {
+//    axios({
+//        url: 'http://localhost:8080/api/task/findAll',
+//        method: 'GET',
+//    })
+//    .then((res) => {
+//        taskList = res.data.content;
+//        renderTasks();
+//    })
+//    .catch((err) => {
+//        console.log(err);
+//    });
+
+axios.get('http://localhost:8080/api/task/findAll')
+  .then(function (res) {       
         taskList = res.data.content;
         renderTasks();
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  })
+  .catch(function (error) {   
+    // on error      
+    console.log(error);
+  });
+
 };
 
 
@@ -38,3 +49,20 @@ const fetchTasks = () => {
 fetchTasks(); 
 
 //document.getElementsByClassName("tutor__info__button__giao-lop")[0].addEventListener("click", fetchTasks());
+
+
+
+    var socket = new SockJS('/our-websocket'); //Mở 1 kết nối websocket đến backend
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        //setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/tasks', function (message) { //Đăng kí một desination để backend đẩy data về fe
+            //showMessage(JSON.parse(message.body).content);
+			console.log("xxx");
+			console.log(JSON.parse(message.body).content);
+			fetchTasks(); 
+        });
+    });
+
+
