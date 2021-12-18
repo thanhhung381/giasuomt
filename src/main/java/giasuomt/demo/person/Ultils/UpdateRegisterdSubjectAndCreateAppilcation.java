5443e0dc5be7caff9fd1030d85f87f1dd7fbafde
khangle <79681550+lenguyenkhang1904@users.noreply.github.com>
2021-12-18 14:04:29 +0700

    FixAlgorithm

--- src/main/java/giasuomt/demo/person/Ultils/UpdateRegisterdSubjectAndCreateAppilcation.java
+++ src/main/java/giasuomt/demo/person/Ultils/UpdateRegisterdSubjectAndCreateAppilcation.java
@@ -7,46 +7,78 @@ import giasuomt.demo.person.model.Tutor;
 import giasuomt.demo.task.model.Application;
 
 public class UpdateRegisterdSubjectAndCreateAppilcation {
-	
-	public static Tutor generateSubjectGroupMaybeInTutor(Tutor tutor)
-	{
-		String subjectGroupByRegisteredSubject="";
-		
-		String subjectGroupByApplication="";
-		
-		List<Subject> allRegisterdSubjects=tutor.getRegisteredSubjects();
-		for(int i=0;i<allRegisterdSubjects.size();i++)
-		{
-			subjectGroupByRegisteredSubject=subjectGroupByRegisteredSubject.concat(allRegisterdSubjects.get(i).getSubjectGroup().getShortName());
-		
+
+	public static Tutor generateSubjectGroupMaybeInTutor(Tutor tutor) {
+		String subjectGroupByRegisteredSubject = "";
+
+		String subjectGroupByApplication = "";
+
+		List<Subject> allRegisterdSubjects = tutor.getRegisteredSubjects();
+		for (int i = 0; i < allRegisterdSubjects.size() - 1; i++) {
+			boolean check = false; // không có phần tử trùng
+			for (int j = 0; j < i; j++) {
+				if (allRegisterdSubjects.get(i).getSubjectGroup().getShortName()
+						.contains(allRegisterdSubjects.get(j).getSubjectGroup().getShortName())) {
+					check = true; // có phần tử
+					break;
+				}
+			}
+
+			if (check == true) {
+				for (int j = i + 1; j < allRegisterdSubjects.size(); j++) {
+
+					if (allRegisterdSubjects.get(i).getSubjectGroup().getShortName()
+							.contains(allRegisterdSubjects.get(j).getSubjectGroup().getShortName())) {
+
+						allRegisterdSubjects.remove(j).getSubjectGroup().getShortName();
+						j--;
+					}
+
+				}
+
+			}
+			subjectGroupByRegisteredSubject = subjectGroupByRegisteredSubject
+					.concat(allRegisterdSubjects.get(i).getSubjectGroup().getShortName() + " ");
+
 		}
-		
 		System.out.println(subjectGroupByRegisteredSubject);
-		List<Application> applications=tutor.getApplications();
-		if(!applications.isEmpty())
-		{
-			for(int j=0;j<applications.size();j++)
-			{
-				List<Subject> subjectFromTask=applications.get(j).getTask().getSubjects();
-				for(int k=0;k<subjectFromTask.size();k++)
-				{
-					subjectGroupByApplication.concat(subjectFromTask.get(k).getSubjectGroup().getShortName());
+
+		List<Application> applications = tutor.getApplications();
+		if (!applications.isEmpty()) {
+			for (int j = 0; j < applications.size(); j++) {
+				List<Subject> subjectFromTask = applications.get(j).getTask().getSubjects();
+				for (int k = 0; k < subjectFromTask.size() - 1; k++) {
+
+					boolean check = false; // không có phần tử trùng
+					for (int l = 0; l < k; l++) {
+						if (subjectFromTask.get(k).getSubjectGroup().getShortName()
+								.contains(subjectFromTask.get(l).getSubjectGroup().getShortName())) {
+							check = true;// có phần tử
+							break;
+						}
+
+					}
+
+					if (check == true) {
+						for (int l = k + 1; l < subjectFromTask.size(); l++) {
+							if (subjectFromTask.get(k).getSubjectGroup().getShortName()
+									.contains(subjectFromTask.get(l).getSubjectGroup().getShortName())) {
+								subjectFromTask.remove(l).getSubjectGroup().getShortName();
+								l--;
+							}
+						}
+					}
+
+					subjectGroupByApplication = subjectGroupByApplication
+							.concat(subjectFromTask.get(k).getSubjectGroup().getShortName() + " ");
 				}
 			}
 		}
-		
-		if(!subjectGroupByApplication.contains(subjectGroupByRegisteredSubject))
-		{
-			tutor.setSubjectGroupMaybe(subjectGroupByRegisteredSubject.concat(",".concat(subjectGroupByApplication)));
-		}
-		else
-		{
-			tutor.setSubjectGroupMaybe(subjectGroupByRegisteredSubject);
-		}
+
+		tutor.setSubjectGroupMaybe(subjectGroupByRegisteredSubject+subjectGroupByApplication);
 		
 		return tutor;
-		
+
 	}
-	
-	
+
 }
