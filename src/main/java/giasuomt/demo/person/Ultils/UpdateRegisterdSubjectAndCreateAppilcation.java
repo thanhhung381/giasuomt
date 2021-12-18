f638504546624a54253679b43aa8068f7ea5f0ff
khangle <79681550+lenguyenkhang1904@users.noreply.github.com>
2021-12-18 19:40:49 +0700

    xong

--- src/main/java/giasuomt/demo/person/Ultils/UpdateRegisterdSubjectAndCreateAppilcation.java
+++ src/main/java/giasuomt/demo/person/Ultils/UpdateRegisterdSubjectAndCreateAppilcation.java
@@ -14,29 +14,8 @@ public class UpdateRegisterdSubjectAndCreateAppilcation {
 		List<String> tempString = new LinkedList<>();
 
 		List<Subject> allRegisterdSubjects = tutor.getRegisteredSubjects();
-		for (int i = 0; i < allRegisterdSubjects.size() - 1; i++) {
-			boolean check = false; // không có phần tử trùng
-			for (int j = 0; j < i; j++) {
-				if (allRegisterdSubjects.get(i).getSubjectGroup().getShortName()
-						.contains(allRegisterdSubjects.get(j).getSubjectGroup().getShortName())) {
-					check = true; // có phần tử
-					break;
-				}
-			}
+		for (int i = 0; i < allRegisterdSubjects.size(); i++) {
 
-			if (check == true) {
-				for (int j = i + 1; j < allRegisterdSubjects.size(); j++) {
-
-					if (allRegisterdSubjects.get(i).getSubjectGroup().getShortName()
-							.contains(allRegisterdSubjects.get(j).getSubjectGroup().getShortName())) {
-
-						allRegisterdSubjects.remove(j);
-						j--;
-					}
-
-				}
-
-			}
 			tempString.add(allRegisterdSubjects.get(i).getSubjectGroup().getShortName());
 		}
 
@@ -44,27 +23,7 @@ public class UpdateRegisterdSubjectAndCreateAppilcation {
 		if (!applications.isEmpty()) {
 			for (int j = 0; j < applications.size(); j++) {
 				List<Subject> subjectFromTask = applications.get(j).getTask().getSubjects();
-				for (int k = 0; k < subjectFromTask.size() - 1; k++) {
-
-					boolean check = false; // không có phần tử trùng
-					for (int l = 0; l < k; l++) {
-						if (subjectFromTask.get(k).getSubjectGroup().getShortName()
-								.contains(subjectFromTask.get(l).getSubjectGroup().getShortName())) {
-							check = true;// có phần tử
-							break;
-						}
-
-					}
-
-					if (check == true) {
-						for (int l = k + 1; l < subjectFromTask.size(); l++) {
-							if (subjectFromTask.get(k).getSubjectGroup().getShortName()
-									.contains(subjectFromTask.get(l).getSubjectGroup().getShortName())) {
-								subjectFromTask.remove(l);
-								l--;
-							}
-						}
-					}
+				for (int k = 0; k < subjectFromTask.size(); k++) {
 
 					tempString.add(subjectFromTask.get(k).getSubjectGroup().getShortName());
 				}
@@ -77,10 +36,10 @@ public class UpdateRegisterdSubjectAndCreateAppilcation {
 
 	}
 
-	private static List<String> removeDuplicateElenmet(List<String> string) {
+	public static List<String> removeDuplicateElenmet(List<String> string) {
 		List<String> temp = new LinkedList<>();
 
-		for (int i = 0; i < string.size() - 1; i++) {
+		for (int i = 0; i < string.size(); i++) {
 			boolean check = false;
 			for (int j = 0; j < i; j++) {
 				if (string.get(i).contains(string.get(j))) {
@@ -89,16 +48,17 @@ public class UpdateRegisterdSubjectAndCreateAppilcation {
 				}
 			}
 
-			if (check == true) {
+			if (check == false) {
+
 				for (int j = i + 1; j < string.size(); j++) {
 					if (string.get(i).contains(string.get(j))) {
+
 						string.remove(j);
 						j--;
 					}
 				}
+				temp.add(string.get(i));
 			}
-
-			temp.add(string.get(i));
 
 		}
 
