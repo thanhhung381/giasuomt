133499bdfe1ee6bd4d684257542f75f0eb25533c
thanhhung381 <thanhhung.vl.381@gmail.com>
2021-12-27 11:43:38 +0700

    new

--- src/main/java/giasuomt/demo/DemoApplication.java
+++ src/main/java/giasuomt/demo/DemoApplication.java
@@ -6,10 +6,17 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Date;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import giasuomt.demo.commondata.generator.TaskCodeGenerator;
 import giasuomt.demo.commondata.generic.StringUltilsForAreaID;
--- src/main/java/giasuomt/demo/security/config/SecurityConfig.java
+++ src/main/java/giasuomt/demo/security/config/SecurityConfig.java
@@ -1,21 +1,35 @@
 package giasuomt.demo.security.config;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+
 
 
 @Configuration  //để nó biết đây là 1 file config
 @EnableWebSecurity  //để nó biết đây là config cho Spring Security
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
 	@Override //Mình Override lại thằng configure(HttpSecurity http) (có sẵn trong Spring Security) -> để mình config việc nó security trên các cái Http Request của mình.
 	protected void configure(HttpSecurity http) throws Exception {
 		//Enable CORS - xem lại tài liệu để hiểu CORS là gì - nói ngắn gọn là ở đây mình cấu hình để cho phép nguồn nào (domain nào) có thể gửi HttpRequest tới Server của mình được, và có thể gởi Request tới API nào được, API nào không.
 		http.cors();  //enable filter cors
 		http.csrf().disable();  //disable csrf tạm thời để mình dev //để ko bị báo lỗi 403 với các api create
-	    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()); //enable cors
+
+	    //http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues().setAllowedMethods(allowedMethods)); //enable cors
+	    
+	    http.cors().configurationSource(request -> {
+	    	CorsConfiguration cors = new CorsConfiguration();
+//	        cors.setAllowedOrigins(List.of("http://localhost:4200", "http://127.0.0.1:80", "http://example.com"));
+	        cors.setAllowedOrigins(List.of("*"));
+	        cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+	        cors.setAllowedHeaders(List.of("*"));
+	        return cors;
+	      }); //enable cors
 
 		//config authentication (xác thực) cho các API. Chứ chưa làm đến việc authorization (phân quyền) cho các API.
 		http.antMatcher("/api/**").authorizeRequests() 
@@ -27,9 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 	}
 	
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
+	
 	
 
 }
