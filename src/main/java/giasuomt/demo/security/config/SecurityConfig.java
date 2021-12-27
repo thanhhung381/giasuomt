15eecc94cc2826806b9d6945b9ac2ee35d83a68b
thanhhung381 <thanhhung.vl.381@gmail.com>
2021-12-27 11:59:37 +0700

    new 3432

--- src/main/java/giasuomt/demo/security/config/SecurityConfig.java
+++ src/main/java/giasuomt/demo/security/config/SecurityConfig.java
@@ -24,8 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	    
 	    http.cors().configurationSource(request -> {
 	    	CorsConfiguration cors = new CorsConfiguration();
-//	        cors.setAllowedOrigins(List.of("http://localhost:4200", "http://127.0.0.1:80", "http://example.com"));
-	        cors.setAllowedOrigins(List.of("*"));
+	        cors.setAllowedOrigins(List.of("http://localhost:8080", "http://127.0.0.1:5500/", "http://example.com"));
 	        cors.setAllowedMethods(List.of("*"));
 	        cors.setAllowedHeaders(List.of("*"));
 	        return cors;
