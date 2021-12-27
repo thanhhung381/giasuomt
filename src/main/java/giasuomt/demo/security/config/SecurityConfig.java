af1617a99fad36fadcbadc3e244e791fd05857a5
thanhhung381 <thanhhung.vl.381@gmail.com>
2021-12-27 11:18:49 +0700

    new

--- src/main/java/giasuomt/demo/security/config/SecurityConfig.java
+++ src/main/java/giasuomt/demo/security/config/SecurityConfig.java
@@ -27,5 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 	}
 	
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+	
 
 }
